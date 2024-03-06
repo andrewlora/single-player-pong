96d7bfd971ee5208a0e1fb16b3cd3be5bcc927e9
Andres Lora <jalc.andres.lora@gmail.com>
2024-03-06 13:35:07 -0700

    Added namespaces (pong) with socket.io

--- sockets.js
+++ sockets.js
@@ -1,12 +1,13 @@
 let readyPlayerCount = 0;
 function listen(io) {
-  io.on('connection', (socket) => {
+  const pongNamespace = io.of('/pong');
+  pongNamespace.on('connection', (socket) => {
     console.log('a user connected', socket.id);
     socket.on('ready', () => {
       console.log('Player ready', socket.id);
       readyPlayerCount++;
       if (readyPlayerCount % 2 === 0) {
-        io.emit('startGame', socket.id); // to all connected clients
+        pongNamespace.emit('startGame', socket.id); // to all connected clients
       }
     });
     socket.on('paddleMove', (paddleData) => {
