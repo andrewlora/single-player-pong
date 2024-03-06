015520dac58697a57889b9980135cc1595af49ac
Andres Lora <jalc.andres.lora@gmail.com>
2024-03-06 14:42:28 -0700

    Added rooms into project with socket.io

--- sockets.js
+++ sockets.js
@@ -3,21 +3,27 @@ function listen(io) {
   const pongNamespace = io.of('/pong');
   pongNamespace.on('connection', (socket) => {
     console.log('a user connected', socket.id);
+    let room;
     socket.on('ready', () => {
-      console.log('Player ready', socket.id);
+      room = 'room' + Math.floor(readyPlayerCount / 2);
+      socket.join(room);
+      console.log(`Player ready ${socket.id} into a room ${room}`);
       readyPlayerCount++;
       if (readyPlayerCount % 2 === 0) {
-        pongNamespace.emit('startGame', socket.id); // to all connected clients
+        pongNamespace.in(room).emit('startGame', socket.id); // to all connected clients
       }
     });
     socket.on('paddleMove', (paddleData) => {
-      socket.broadcast.emit('paddleMove', paddleData); // except the sender
+      // socket.broadcast.emit('paddleMove', paddleData); // except the sender
+      socket.to(room).emit('paddleMove', paddleData); // except the sender into the room
     });
     socket.on('ballMove', (ballData) => {
-      socket.broadcast.emit('ballMove', ballData); // except the sender
+      // socket.broadcast.emit('ballMove', ballData); // except the sender
+      socket.to(room).emit('ballMove', ballData); // except the sender into the room
     });
     socket.on('disconnect', (reason) => {
       console.log(`Client ${socket.id} disconnected: ${reason}`);
+      socket.leave(room);
     });
   });
 }
